f3689abda86ff99f07dfb4f4e4952b504626c5b6
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-13 19:50:03 +0200

    further revise the implementation of redisStorage by adding return types

--- src/storage/redis.ts
+++ src/storage/redis.ts
@@ -31,17 +31,17 @@ const lrem = promisify(client.lrem).bind(client);
 const lrange = promisify(client.lrange).bind(client);
 
 export const redisStorage: Interfaces.IStorage = {
-  get: (list: string) => {
+  get: (list: string): Promise<string[]> => {
     return lrange(list, 0, -1)
       .then((val: string[]) => val)
       .catch((e: Error) => []);
   },
-  add: (list: string, name: string) => {
+  add: (list: string, name: string): Promise<boolean> => {
     return rpush(list, name)
       .then((val: number) => val > 0)
       .catch((e: Error) => false);
   },
-  remove: (list: string, name: string) => {
+  remove: (list: string, name: string): Promise<boolean> => {
     return lrem(list, 0, name)
       .then((val: number) => val > 0)
       .catch((e: Error) => false);
