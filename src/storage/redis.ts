dabd194702117eed43635548b2cc1833e85c011f
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-13 19:50:03 +0200

    make the mocking of the storage implementation into a complete success (by converting redisStorage from a JS object into a function that returns a JS object)

--- src/storage/redis.ts
+++ src/storage/redis.ts
@@ -1,49 +1,50 @@
 import * as Interfaces from "../types/interfaces";
 import { config } from "../config";
-
 const redis = require("redis"); /* Why `require` and not `import`? Simply, `import` hits on some issues with TypeScript complaining - can't tell what the reason behind that was. */
-const client = redis.createClient(config.redis);
-
 /*
 [The following is] a utility provided by Node that takes an existing function
 and turns it into a `Promise`-based version of that function.
 */
 const { promisify } = require("util");
 
-/*
-Because we're re-defining [parts of functionality provided by the `redis` library]
-[on the right hand sides of the next instructions],
-we need to make sure that what this function knows as `this` is what we expect it to be;
+export function redisStorage(): Interfaces.IStorage {
+  const client = redis.createClient(config.redis);
 
-in other words, when our `rpush`, `lrem`, or `lrange` is called against
-something that we call the `client`, we need to make sure that that something is
-actually the `client`.
+  /*
+  Because we're re-defining [parts of functionality provided by the `redis` library]
+  [on the right hand sides of the next instructions],
+  we need to make sure that what this function knows as `this` is what we expect it to be;
 
-[
-For that, we can call `bin` passing in the `client`,
-which will become the `this` on this newly-created function.
+  in other words, when our `rpush`, `lrem`, or `lrange` is called against
+  something that we call the `client`, we need to make sure that that something is
+  actually the `client`.
 
-... has to be one of JavaScript's weirder quirks.
-]
-*/
-const rpush = promisify(client.rpush).bind(client);
-const lrem = promisify(client.lrem).bind(client);
-const lrange = promisify(client.lrange).bind(client);
+  [
+  For that, we can call `bin` passing in the `client`,
+  which will become the `this` on this newly-created function.
 
-export const redisStorage: Interfaces.IStorage = {
-  get: (list: string): Promise<string[]> => {
-    return lrange(list, 0, -1)
-      .then((val: string[]) => val)
-      .catch((e: Error) => []);
-  },
-  add: (list: string, name: string): Promise<boolean> => {
-    return rpush(list, name)
-      .then((val: number) => val > 0)
-      .catch((e: Error) => false);
-  },
-  remove: (list: string, name: string): Promise<boolean> => {
-    return lrem(list, 0, name)
-      .then((val: number) => val > 0)
-      .catch((e: Error) => false);
-  }
-};
+  ... has to be one of JavaScript's weirder quirks.
+  ]
+  */
+  const rpush = promisify(client.rpush).bind(client);
+  const lrem = promisify(client.lrem).bind(client);
+  const lrange = promisify(client.lrange).bind(client);
+
+  return {
+    get: (list: string): Promise<string[]> => {
+      return lrange(list, 0, -1)
+        .then((val: string[]) => val)
+        .catch((e: Error) => []);
+    },
+    add: (list: string, name: string): Promise<boolean> => {
+      return rpush(list, name)
+        .then((val: number) => val > 0)
+        .catch((e: Error) => false);
+    },
+    remove: (list: string, name: string): Promise<boolean> => {
+      return lrem(list, 0, name)
+        .then((val: number) => val > 0)
+        .catch((e: Error) => false);
+    }
+  };
+}
