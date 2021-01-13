2f328c7f27201e8cf07e4d0d66ab207b147c1e68
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-13 19:50:03 +0200

    prove that the add function works, but the tests are only in a "somewhat passing state" because, as soon as we re-run the tests, we are going to add another item to the list and that test is going to start failing again

--- __tests__/storage/redis.test.ts
+++ __tests__/storage/redis.test.ts
@@ -3,7 +3,7 @@ The following important remarks about this file are in order:
 
 - not TDD
   but is-the-prototype-working-driven tests
-  
+
 - not a true unit tests because [this unit testing] relies on Redis,
   (i.e. Redis needs to be available when [this file's] tests are run);
   but the process can be an interesting discovery exercise
@@ -12,21 +12,46 @@ The following important remarks about this file are in order:
 import { redisStorage } from "../../src/storage/redis";
 
 describe("storage/redis", () => {
-  describe("get", () => {
-    it("should initially return an empty list", async () => {
-      const list_name = "my_test_list";
+  /*
+  Whilst the following constant is going to be available to all our tests,
+  under the hood Redis is going to be re-using this list per test.
+  
+  So what this is means is
+  each of the tests is going to rely on the previous test
+  - and that is NOT GOOD.
 
+  This then raises a bunch of questions as to how you would effectively test this.
+  [The tutorial author's] personal approach would probably be towards integration
+  testing (so testing this sort of "a layer above" unit tests), but it's really hard to
+  say generically how to test this or indeed if you even should...
+  that's why testing is such a difficult subject beyond just learning the syntax.
+  */
+  // const list_name = "my_test_list";
+
+  describe("get", () => {
+    const list_name = "get_test_list";
+
+    it("should initially return an empty list", async () => {
+      expect(await redisStorage.get(list_name)).toEqual([]);
+      /* is equivalent to */
+      // expect(redisStorage.get(list_name)).resolves.toEqual([]);
+    });
+
+    xit("should return all items in a list", async () => {
       expect(await redisStorage.get(list_name)).toEqual([]);
       /* is equivalent to */
       // expect(redisStorage.get(list_name)).resolves.toEqual([]);
     });
   });
 
-  xdescribe("add", () => {
-    /*
-    The 'x' before 'describe' prevents this test from being run.
-    This is needed for the time being.
-    */
+  describe("add", () => {
+    const list_name = "add_test_list";
+
+    it("should allow adding an item to a list", async () => {
+      expect(await redisStorage.add(list_name, "chris")).toBeTruthy();
+
+      expect(await redisStorage.get(list_name)).toEqual(["chris"]);
+    });
   });
   xdescribe("remove", () => {
     /*
