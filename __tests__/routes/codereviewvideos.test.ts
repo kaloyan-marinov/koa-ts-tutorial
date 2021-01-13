f2f88f739bacca926f3d35529319475107ec1ce7
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-13 19:50:03 +0200

    as another demonstration of test-driven development, create a test which (fails but) proves that there exists the following bug: if a client makes 2 POST requests, the response to the 2nd one returns a list containing the 2nd game but not the 1st one

--- __tests__/routes/codereviewvideos.test.ts
+++ __tests__/routes/codereviewvideos.test.ts
@@ -1,3 +1,4 @@
+import { executionAsyncId } from "async_hooks";
 import server from "../../src/server";
 const request = require("supertest");
 
@@ -20,6 +21,30 @@ describe("routes/codereviewvideos", () => {
       expect(response.body).toEqual({
         games: [game]
       });
+    });
+  });
+
+  it("should keep track of all games added to the list", async () => {
+    const data1 = { game: "Half Life 3" };
+    const response1 = await request(server)
+      .post("/codereviewvideos")
+      .send(data1);
+
+    expect(response1.status).toEqual(201);
+    expect(response1.type).toEqual("application/json");
+    expect(response1.body).toEqual({
+      games: [data1.game]
+    });
+
+    const data2 = { game: "Grand Theft Auto" };
+    const response2 = await request(server)
+      .post("/codereviewvideos")
+      .send(data2);
+
+    expect(response2.status).toEqual(201);
+    expect(response2.type).toEqual("application/json");
+    expect(response2.body).toEqual({
+      games: [data1.game, data2.game]
     });
   });
 });
